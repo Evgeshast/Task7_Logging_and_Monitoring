b11107ea16637b0d31ea8e8c8813646781f9a963
Yauheniya Strelkova <Yauheniya_Strelkova@epam.com>
2020-04-17 17:09:43 +0300

    Add project files.

---
+++ MvcMusicStore/Infrastructure/Counters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using PerformanceCounterHelper;
+
+namespace MvcMusicStore.Infrastructure
+{
+    [PerformanceCounterCategory("MvcMusicStore", System.Diagnostics.PerformanceCounterCategoryType.MultiInstance, "MvcMusicStore")]
+    public enum Counters
+    {
+        [PerformanceCounter("Successful login count", "Successful login", PerformanceCounterType.NumberOfItems32)]
+        SuccessfulLogin,
+        [PerformanceCounter("Unsuccessful login count", "Unsuccessful login", PerformanceCounterType.NumberOfItems32)]
+        UnsuccessfulLogin,
+        [PerformanceCounter("User registration count", "User registration", PerformanceCounterType.NumberOfItems32)]
+        UserRegistration
+    }
+}
